7db644a3638f0226afc4cf5ec70d47b64adb6e96
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-01-31 20:21:42 -0300

    styles: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
 
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
 }
