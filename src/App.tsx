8d7846b6d04154a77d9241d43609cf99d10cb3bd
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-02-01 21:49:53 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyles />
       </ThemeProvider>
     </>
